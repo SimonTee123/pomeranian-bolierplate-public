4059cf7571b4cc43b29c9f8ceefeb773a5fad392
SimonTee123 <simon.tumielewicz@gmail.com>
2023-08-23 19:42:08 +0200

    Logo

---
+++ src/App/Components/Logo/Logo.jsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+import logo from '../../Images/start-it-logo.svg';
+
+export const Logo = () => {
+  return (
+    <div>
+      <img className="Logo" src={logo} />
+    </div>
+  );
+};
