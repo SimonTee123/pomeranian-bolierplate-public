c1c52efd04070335e31238ef8321bf255d49b05c
SimonTee123 <simon.tumielewicz@gmail.com>
2023-08-24 20:37:02 +0200

    Counter

--- src/App/Exercises/ReactExercises/OnClick/OnCLick.jsx
+++ src/App/Exercises/ReactExercises/OnClick/OnCLick.jsx
@@ -2,21 +2,23 @@ import { useState } from 'react';
 import './styles.css';
 
 export const OnClick = () => {
-  const [isClicked, setIsClicked] = useState(false);
-  const text = isClicked ? 'Kliknięte' : 'Nie kliknięte';
+  //const [isClicked, setIsClicked] = useState(false);
+  //const text = isClicked ? 'Kliknięte' : 'Nie kliknięte';
+  const [count, setCount] = useState(0);
 
   function handleClick() {
-    if (isClicked === true) {
+    /* if (isClicked === true) {
       setIsClicked(false);
     } else {
       setIsClicked(true);
-    }
+       } */
+    setCount(count + 1);
   }
   return (
     <div>
       <h1>Cześć!</h1>
 
-      <button onClick={handleClick}>{text}</button>
+      <button onClick={handleClick}>{count}</button>
     </div>
   );
 };
