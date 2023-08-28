d7c68872bd3e5425fff6d84522cfdfdac56e9d1f
SimonTee123 <simon.tumielewicz@gmail.com>
2023-08-28 20:47:10 +0200

    28.08

---
+++ src/App/Exercises/HtmlCss/SelectorsAndCascade/SelectorsAndCascade.jsx
@@ -0,0 +1,18 @@
+import './styles.css';
+
+export const SelectorsAndCascade = () => {
+  return (
+    <>
+      <div>
+        <span>Pierwszy</span>
+        <span>Drugi</span>
+        <span>Trzeci</span>
+        <span>Czwarty</span>
+      </div>
+
+      <button className="hover">Hover</button>
+      <input type="text" className="firstname" />
+      <div className="pseudo">Pseudo</div>
+    </>
+  );
+};
---
+++ src/App/Exercises/ReactExercises/MoreOrLess/MoreOrLess.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+export const MoreOrLess = () => {
+  const [luckyNumber, setLuckyNumber] = useState(' ');
+  const [hasErrors, setHasErrors] = useState(false);
+  const HIDDEN_NUMBER = 8;
+
+  function handleChange(event) {
+    setHasErrors(false);
+    setLuckyNumber(event.target.value);
+
+    if (isNaN(+event.target.value)) {
+      setHasErrors(true);
+    }
+  }
+
+  const isBigger = +luckyNumber > HIDDEN_NUMBER;
+
+  return (
+    <div>
+      <p>Podaj liczbę i sprawdź czy jest większa od ukrytej liczby</p>
+      <input type="text" value={luckyNumber} onChange={handleChange} />
+      {luckyNumber && <p>Odpowiedź: {isBigger ? 'TAK' : 'NIE'}</p>}
+    </div>
+  );
+};
